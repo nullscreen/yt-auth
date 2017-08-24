8c1fd162719727ff5e48a4ee5bce8213856d4270
claudiob <claudiob@users.noreply.github.com>
2017-08-24 13:48:53 -0700

    Add generation of access token from refresh token

--- spec/email_spec.rb
+++ spec/email_spec.rb
@@ -34,4 +34,13 @@ describe 'Yt::Auth#email' do
       expect(JWT).to receive(:decode).and_return [{'email' => email}]
     end
   end
+
+  context 'given an invalid refresh token' do
+    invalid = 'Invalid refresh token.'
+
+    it 'raises an error' do
+      auth = Yt::Auth.new refresh_token: rand(36**20).to_s(36)
+      expect{auth.email}.to raise_error Yt::HTTPError, invalid
+    end
+  end
 end
